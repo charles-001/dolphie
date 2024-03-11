f61ca460472da1975dbc74c14a3611643f0020d6
Charles Thompson <cthompson@digitalocean.com>
2024-03-11 06:12:00 -0400

    Catch pymysql.OperationalError so it doesn't crash

--- dolphie/Modules/MySQL.py
+++ dolphie/Modules/MySQL.py
@@ -61,7 +61,8 @@ class Database:
 
             # Determine if SSL is being used
             self.using_ssl = "ON" if self.fetch_value_from_field("SHOW STATUS LIKE 'Ssl_cipher'", "Value") else "OFF"
-
+        except pymysql.OperationalError as e:
+            raise ManualException(e)
         except pymysql.Error as e:
             raise ManualException(e.args[1])
         except FileNotFoundError:  # Catch SSL file path errors
