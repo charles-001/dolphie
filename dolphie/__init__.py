185224ffa7c0a1388e086b0a9ba5bd66e9d0bb5d
silver <109951843+silverlee425@users.noreply.github.com>
2024-04-09 11:51:42 +0900

    Add 'not self.aws_rds' to the message_metric_data() function.
    Aurora MySQL there is no BACKUP_ADMIN privilege

--- dolphie/__init__.py
+++ dolphie/__init__.py
@@ -274,8 +274,8 @@ class Dolphie:
         if self.is_mysql_version_at_least("8.0"):
             # If we're using MySQL 8, we need to fetch the checkpoint age from the performance schema if it's not
             # available in global status
-            # On Azure MySQL there is no BACKUP_ADMIN privilege so we can't fetch the checkpoint age
-            if not self.global_status.get("Innodb_checkpoint_age") and not self.azure:
+            # On Azure MySQL and Aurora MySQL there is no BACKUP_ADMIN privilege so we can't fetch the checkpoint age
+            if not self.global_status.get("Innodb_checkpoint_age") and not self.azure and not self.aws_rds:
                 self.global_status["Innodb_checkpoint_age"] = self.main_db_connection.fetch_value_from_field(
                     MySQLQueries.checkpoint_age, "checkpoint_age"
                 )
