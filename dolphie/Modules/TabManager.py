8fe3fc4a4598a22553a3b713f9d23222be6ccb3b
Eduardo Ortega <5791035+ejortegau@users.noreply.github.com>
2025-05-01 16:54:42 +0200

    Add Current/Instantaneous view of to events_statements_summary panel

--- dolphie/Modules/TabManager.py
+++ dolphie/Modules/TabManager.py
@@ -512,6 +512,7 @@ class TabManager:
                             [
                                 RadioButton("Delta since panel opened", id="statements_summarys_delta", value=True),
                                 RadioButton("Total since MySQL restart", id="statements_summary_total"),
+                                RadioButton("Delta since last sample", id="statements_summary_delta_last_sample"),
                             ]
                         ),
                         id="statements_summary_radio_set",
